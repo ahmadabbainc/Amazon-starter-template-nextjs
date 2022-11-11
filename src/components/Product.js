7357eac28567ed563e2975c4e1151be2ec836fe3
Ahmad Inc <111195626+ahmadabbainc@users.noreply.github.com>
2022-11-11 18:30:22 +0100

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Currency from 'react-currency-formatter';
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
+const Product = ({ id, title, price, description, category, image }) => {
+    const [rating] = useState(
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+    );
+    const [hasPrime] = useState(Math.random() < 0.5)
+    return (
+        <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
+            <p className='absolute top-2 right-2 italic text-xs text-gray-400'>{category}</p>
+
+            <Image src={image} height={200} width={200} objectFit="contain" />
+
+            <h4 className='my-3'>{title}</h4>
+
+            <div className='flex'>
+                {Array(rating).fill().map((_, i) => (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-yellow-500">
+                        <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
+                    </svg>
+                ))}
+            </div>
+
+            <p className='text-xs my2 line-clamp-2'>{description}</p>
+
+            <div className='mb-5'>
+                <Currency
+                    quantity={price}
+                    currency="USD"
+                />
+            </div>
+
+            {hasPrime && (
+                <div className='flex items-center space-x-2 -mt-5'>
+                    <img className='w-12' src="https://images-na.ssl-images-amazon.com/images/G/01/prime/delivery-landingpage/prime_logo_new._V524737024_.jpg" alt="prime" />
+                    <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
+                </div>
+            )}
+
+            <button className='mt-auto button'>Add to Cart</button>
+        </div>
+    )
+}
+
+export default Product
---
+++ src/components/ProductsFeed.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Product from './Product'
+
+const ProductsFeed = ({ products }) => {
+    return (
+        <div className='grid grid-flow-row md:grid-cols-2 lg:cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+            {products.slice(0,4).map(({ id, title, price, description, category, image }) =>
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            )}
+            <img className='md:col-span-full w-full' src="https://scontent.flos3-2.fna.fbcdn.net/v/t39.30808-6/314406732_129628556557827_1628853690282684200_n.png?stp=dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=ab6a21&_nc_eui2=AeFl1FFkMlWR3SFCG-beku3EzaSH7NI1-8HNpIfs0jX7wdN7_59Eqg9esTBcDSj3WjwfjuF7Di_5_AKPRSRcZBJv&_nc_ohc=S4QC0jXVjNAAX_PTUfk&_nc_zt=23&_nc_ht=scontent.flos3-2.fna&oh=00_AfBR0nWA4c2Kg85WRjNfrVXYjvW31AL8FMC6krRACu8miQ&oe=6373AB43" alt="feed-banner" />
+            <div className='md:col-span-2'>
+            {products.slice(4, 5).map(({ id, title, price, description, category, image }) =>
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            )}
+            </div>
+            {products.slice(5, products.length).map(({ id, title, price, description, category, image }) =>
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            )}
+        </div>
+    )
+}
+
+export default ProductsFeed
